5c94ff4ee92c2b0f7ae46e151700b6e52fc6b674
harsh-env <mpddriller@live.com>
2022-07-08 18:48:03 -0500

    added color code for all Basins on Leaflet

--- code/static/js/logic.js
+++ code/static/js/logic.js
@@ -11,40 +11,24 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(myMap);
   
   // Use this link to get the GeoJSON data.
-//   var link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
 var link = "https://gist.githubusercontent.com/seasmith/5d4eaa8c752dc6223315a5dc7b2798f2/raw/825bed52bd6612ab03f62d730573c7a8fdcf0aad/us_basins.json";
 
-// var basin_feature
-// var basin_name
-
-// // Creating a GeoJSON layer with the retrieved data
-// d3.json(link).then(function(data) {
-//     basin_feature = data.features
-//     for (let i=0; i < basin_feature.lenght; i++){
-//         let basin_name = features[i].properties.thing; 
-//         if (basin_name == "Haynesville") {
-//             data1 = features[i].geomtery.coordinates;
-//             L.geoJson(data1).addTo(myMap);
-//         }
-//     }
-    
-// });
  // Our style object
 function chooseColor(thing) {
     if (thing == "Ardmore Woodford") return "yellow";
-    else if (thing == "Arkoma Woodford") return "red";
+    else if (thing == "Arkoma Woodford") return "yellow";
     else if (thing == "Barnett") return "orange";
-    else if (thing == "Cana Woodford") return "green";
-    else if (thing == "DJ-Niobrara") return "purple";
-    else if (thing == "Eagle Ford") return "purple";
-    else if (thing == "Fayetteville") return "purple";
-    else if (thing == "Granite Wash") return "purple";
+    else if (thing == "Cana Woodford") return "yellow";
+    else if (thing == "DJ-Niobrara") return "black";
+    else if (thing == "Eagle Ford") return "pink";
+    else if (thing == "Fayetteville") return "brown";
+    else if (thing == "Granite Wash") return "chartreuse";
     else if (thing == "Haynesville") return "purple";
-    else if (thing == "Marcellus") return "purple";
-    else if (thing == "Mississippian") return "purple";
-    else if (thing == "Permian") return "brown";
+    else if (thing == "Marcellus") return "blue";
+    else if (thing == "Mississippian") return "violet";
+    else if (thing == "Permian") return "green";
     else if (thing == "Utica") return "red";
-    else if (thing == "Williston") return "green";
+    else if (thing == "Williston") return "aqua";
     // else if (thing == "others") return "brown";
     else return "none";
 }
@@ -54,9 +38,9 @@ d3.json(link).then(function(data) {
     L.geoJson(data, {
         style: function(feature){
             return {
-                color: "black",
-                fillcolor: chooseColor(feature.properties.thing),
-                fillOpacity: 0.05,
+                color: chooseColor(feature.properties.thing),
+                // fillcolor: chooseColor(feature.properties.thing),
+                fillOpacity: 0.5,
                 weight: 1.5
             };
         }
