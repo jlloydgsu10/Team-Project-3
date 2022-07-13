7c26954e78a6e0a3d219d861110683d3fea5e86e
harsh-env <mpddriller@live.com>
2022-07-12 20:27:10 -0500

    update 7 12th on plotly and data

--- code/static/js/logic.js
+++ code/static/js/logic.js
@@ -1,7 +1,7 @@
 // Creating the map object
 var myMap = L.map("map", {
-    center: [39.997940, -102.050894],
-    zoom: 4.6
+    center: [39, -97],
+    zoom: 5.4959
 
 });
   
@@ -53,6 +53,42 @@ d3.json(link).then(function(data) {
         }
     }).addTo(myMap);
 
+    var marker = new L.marker([27.237698, -99.473009], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Eagle Ford", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([32.160709, -92.839724], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Haynesville", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([31.632453, -99.893885], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Barnett", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([32.889025, -104.310934], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Permian", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([35.404664, -90.132335], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Fayetteville", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([36.922401, -95.338540], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Mississippian", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([34.922401, -105.838540], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Granite Wash", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([35.922401, -80.838540], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Marcellus", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([40.922401, -72.838540], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Utica", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([40.922401, -103.958540], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("DJ-Niobrara", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([45.922401, -102.958540], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip("Williston", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
+    var marker = new L.marker([33.922401, -96.838540], { opacity: 0.01 }); //opacity may be set to zero
+    marker.bindTooltip(" Woodford", {permanent: true, className: "my-label", offset: [0, 0] });
+    marker.addTo(myMap);
     // var marker = L.marker([45.52, -122.67], {
     //     draggable: true,
     //     title: "My First Marker"
@@ -61,7 +97,7 @@ d3.json(link).then(function(data) {
     // marker.bindPopup("Hello There!");
 
     // var myIcon = L.divIcon({className: 'my-div-icon'});
-    // L.marker([45.52, -122.67], {icon: myIcon}).addTo(myMap);
+    // L.marker([45.52, -122.67], {opacity: 0}).addTo(myMap);
   //   var middle = [45.52, -122.67]
   //   L.Control.textbox = L.Control.extend({
 	// 	onAdd: function(myMap) {
@@ -75,9 +111,9 @@ d3.json(link).then(function(data) {
 	// 	onRemove: function(myMap) {
 	// 		// Nothing to do here
 	// 	}
-	// });
+	// }); L.marker - bind it with tooltip()
 	// L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
-	// L.control.textbox({ position: middle }).addTo(myMap);
+	// L.control.textbox({ position: bottomleft }).addTo(myMap);
 
    
 
--- code/static/js/logic1.js
+++ code/static/js/logic1.js
@@ -1,13 +1,42 @@
-// var biden_data = ["biden_approval.csv"];
-// d3.csv("./data/biden_approval.csv").then(function(data) {
-//     console.log(data)   
-// })
-// .catch(function(error){
-//     console.log(error);
-// });
-var link = "C:\BC_Classes\Homeworks\Project3\code\static\data\biden_approval.json";
-// d3.json(link).then(function(data) {
-d3.json("./data/biden_approval.json").then(function(data) {
-    console.log(data)
+var date1=[];
+var rating=[];
+// var rating
+d3.csv("./static/data/biden_approval.csv").then(function(data){
+    data.map(function(d){
+        // console.log(d.Date)
+        // date1 = Date.parse(d.Date);
+        // rating = parseInt(d.Relative_Approval);
+        // date1.push(Date.parse(d.Date));
+        date1.push(Date.parse(d.Date));
+        // date1.push(parseInt(d.Date));
+        rating.push(parseInt(d.Relative_Approval));
+        
+    });
 });
+console.log(date1)
+console.log(rating)
+
+var Trace1 = {
+    // x: unpack(rows, 'Date'),
+    // y: unpack(rows, 'Relative_Approval'),
+    // x: [2,3,4],
+    // y: [4,5,6],
+    x: date1, 
+    y: rating, 
+    // x: [1657256400000, 1657170000000], 
+    // y: [-24, 4],
+    type:"scatter",
+    mode: 'lines'
+    // type:"scatter"
+};
+
+var traceData = [Trace1];
+
+// // Apply the group barmode to the layout
+let layout = {
+  title: "Presidential Approval Rating & Oil Price"
+};
+
+// // Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", traceData, layout);
 
